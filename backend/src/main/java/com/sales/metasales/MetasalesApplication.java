95fcf0d77a401cf38451151e7ab46f2d3a685851
Jefferson da Silva <je_jss@hotmail.com>
2022-07-13 07:48:19 -0300

    Innitial commit - Project Created

---
+++ backend/src/main/java/com/sales/metasales/MetasalesApplication.java
@@ -0,0 +1,13 @@
+package com.sales.metasales;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MetasalesApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MetasalesApplication.class, args);
+	}
+
+}
