aa82f7b32d8cef21f38183252b52757555bdc5f8
Jefferson da Silva <je_jss@hotmail.com>
2022-09-07 14:25:38 -0300

    Include ButtonComponent

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,6 +1,11 @@
+import NotificationButton from "./components/NotificationButton"
+
 function App() {
-  return(
-    <h1>Hello, Dev World</h1>
+  return (
+    <>
+      <h1>Hello, Dev World</h1>
+      <NotificationButton />
+    </>
   )
 }
 
---
+++ frontend/src/components/NotificationButton/index.tsx
@@ -0,0 +1,12 @@
+import icon from '../../assets/img/Notification-icon.svg'
+import './styles.css';
+
+function NotificationButton() {
+    return(
+            <div className="meta-red-btn">
+                <img src={icon} alt="Icone de Notificar" />
+            </div>
+        )
+  }
+  
+  export default NotificationButton
