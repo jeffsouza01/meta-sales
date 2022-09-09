a136b5c2cde09ad201b4d8e9746bcf3226611644
Jefferson da Silva <je_jss@hotmail.com>
2022-09-09 17:04:38 -0300

    Update Config JPA Project

---
+++ backend/src/test/java/com/sales/metasales/MetasalesApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sales.metasales;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MetasalesApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
