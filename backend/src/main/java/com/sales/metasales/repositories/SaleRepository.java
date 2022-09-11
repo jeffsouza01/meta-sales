d87d643aaeb5e409e664a7c96adf8e8ff1dacb3e
Jefferson da Silva <je_jss@hotmail.com>
2022-09-11 17:48:40 -0300

    Include Sale Repository

---
+++ backend/src/main/java/com/sales/metasales/repositories/SaleRepository.java
@@ -0,0 +1,11 @@
+package com.sales.metasales.repositories;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import com.sales.metasales.entities.Sale;
+
+@Repository
+public interface SaleRepository extends JpaRepository<Sale, Long>{
+
+}
