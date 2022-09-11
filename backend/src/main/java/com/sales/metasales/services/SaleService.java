2480710179fa9b2033c427256bedc3f4b8796251
Jefferson da Silva <je_jss@hotmail.com>
2022-09-11 19:22:29 -0300

    Date Select

--- backend/src/main/java/com/sales/metasales/services/SaleService.java
+++ backend/src/main/java/com/sales/metasales/services/SaleService.java
@@ -1,22 +1,42 @@
 package com.sales.metasales.services;
 
-import java.util.List;
+import java.time.Instant;
+import java.time.LocalDate;
+import java.time.ZoneId;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.stereotype.Service;
+import org.springframework.web.bind.annotation.RequestParam;
 
 import com.sales.metasales.entities.Sale;
 import com.sales.metasales.repositories.SaleRepository;
 
 @Service
 public class SaleService {
-	
+
 	@Autowired
 	private SaleRepository saleRepository;
+
+	public Page<Sale> findSales(Pageable pageable) {
+
+		return saleRepository.findAll(pageable);
+
+	}
 	
-	public List<Sale> findSales() {
+	
+	public Page<Sale> findSalesByDate(
+			String minDate,
+			String maxDate,
+			Pageable pageable) {
 		
-		return saleRepository.findAll();
+		LocalDate today = LocalDate.ofInstant(Instant.now(), ZoneId.systemDefault());
+		
+		LocalDate min = minDate.equals("") ? today.minusMonths(12) : LocalDate.parse(minDate);
+		LocalDate max = maxDate.equals("") ? today : LocalDate.parse(maxDate);
+		
+		return saleRepository.findSalesByDate(min, max, pageable);
 		
 	}
 
